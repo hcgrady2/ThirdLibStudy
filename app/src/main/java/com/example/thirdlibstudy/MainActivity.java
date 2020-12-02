49cb1fcdc846abe1189b522f69eed0953103ab57
wanghaichao <hcwang1024@163.com>
2020-12-02 14:38:18 +0800

    自定义注解处理器生成 helloword 类

--- app/src/main/java/com/example/thirdlibstudy/MainActivity.java
+++ app/src/main/java/com/example/thirdlibstudy/MainActivity.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.butterknife_annotations.TestAnotation;
 import com.example.thirdlibstudy.butterknife.ButterknifeMainActivity;
 
+
+@TestAnotation
 public class MainActivity extends AppCompatActivity {
 
     Button btnButterKnife;
